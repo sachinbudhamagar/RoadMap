5ea691217165fc2c89ca86ccfd7223f79c7c3d95
sachinbudhamagar <sachinbmagar19@gmail.com>
2026-01-18 12:30:46 +0900

    input limit exceed function added on .py file

--- Task 1_2/task 1.2.py
+++ Task 1_2/task 1.2.py
@@ -7,8 +7,7 @@ def conflict_security(prompt):
     return any(word in prompt for word in risk)
 
 def prompt_limit(prompt):
-    for prompt in range(3):
-        return prompt
+    return prompt.count("10")
 
 def syntax_error(prompt): 
     return prompt.count("(") != prompt.count(")")
@@ -20,18 +19,18 @@ prompt = input("Enter to reach right path: ").lower()
 
 def validating_prompt(prompt: str):
     failure = []
+    if prompt_limit(prompt):
+        failure.append("Prompt limit exceed")
     if violates_policy(prompt):
         failure.append("Policy violation")
     if conflict_security(prompt):
         failure.append("Security conflict")
-    if prompt_limit(prompt):
-        failure.append("Prompt limit exceed")
     if syntax_error(prompt):
         failure.append("Syntax error")
     if format_error(prompt):
         failure.append("Format error")
     
-    priority = ["Policy violation", "Security conflict", "Prompt limit exceed", "Syntax error", "Format error"]
+    priority = ["Prompt limit exceed", "Policy violation", "Security conflict", "Syntax error", "Format error"]
     for p in priority: #checks error priority order list 
         if p in failure: 
             return p, failure #Exit on 1st priority error
